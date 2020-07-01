ef298be3b63af7056458a4128c6ae96559f71f91
Nikolay Botalov <nikolaj.botalov@gmail.com>
2020-07-01 13:31:32 +0500

    add timer

--- js/script.js
+++ js/script.js
@@ -1,32 +1,33 @@
-window.addEventListener('DOMContentLoaded', () => {
-  'use strict';
+window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
 
-  let tab = document.querySelectorAll('.info-header-tab'),
-      info = document.querySelector('.info-header'),
-      tabContent = document.querySelectorAll('.info-tabcontent'),
+  let tab = document.querySelectorAll(".info-header-tab"),
+    info = document.querySelector(".info-header"),
+    tabContent = document.querySelectorAll(".info-tabcontent"),
+    deadline = "2020-07-04";
 
   // Скрываем контент
   function hideTabContent(a) {
     for (let i = a; i < tabContent.length; i++) {
-      tabContent[i].classList.remove('show');
-      tabContent[i].classList.add('hide');
+      tabContent[i].classList.remove("show");
+      tabContent[i].classList.add("hide");
     }
   }
 
   hideTabContent(1);
 
-  // Отображаем контент 
+  // Отображаем контент
   function showTabContent(b) {
-    if (tabContent[b].classList.contains('hide')) {
-      tabContent[b].classList.remove('hide');
-      tabContent[b].classList.add('show');
+    if (tabContent[b].classList.contains("hide")) {
+      tabContent[b].classList.remove("hide");
+      tabContent[b].classList.add("show");
     }
   }
 
   // Вешаем событие на кнопки, при клике на кнопку, открываем соответствующий таб
-  info.addEventListener('click', (e) => {
-    let target = e.target; 
-    if (target && target.classList.contains('info-header-tab')) {
+  info.addEventListener("click", (e) => {
+    let target = e.target;
+    if (target && target.classList.contains("info-header-tab")) {
       for (let i = 0; i < tab.length; i++) {
         if (target == tab[i]) {
           hideTabContent(0);
@@ -37,6 +38,47 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+  // Реализация таймера обратного отчета
+  function addZero(num) {
+    if (num <= 9) {
+      return '0' + num;
+    } else {
+      return num;
+    }
+  }
 
+  function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+      seconds = Math.floor((t / 1000) % 60),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      hours = Math.floor(t / (1000 * 60 * 60));
 
+    return {
+      'total':    t,
+      'hours':    hours,
+      'minutes':  minutes,
+      'seconds':  seconds
+    };
+  }
+
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+        hours = timer.querySelector(".hours"),
+        minutes = timer.querySelector(".minutes"),
+        seconds = timer.querySelector(".seconds"),
+        timeInterval = setInterval(updateClock, 1000)
+
+    function updateClock() {
+      let t = getTimeRemaining(endtime);
+      hours.textContent = addZero(t.hours);
+      minutes.textContent = addZero(t.minutes);
+      seconds.textContent = addZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock('timer', deadline);
 });
