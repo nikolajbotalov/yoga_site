b6a750bd452981c0b7c52d16f259df9dfe759178
Nikolay Botalov <nikolaj.botalov@gmail.com>
2020-07-01 11:44:59 +0500

    Add project files. Hide & show tabs
    Верстка выполнена не мной. 
    JS написан при прохождения курса на Udemy

---
+++ js/script.js
@@ -0,0 +1,42 @@
+window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+
+  let tab = document.querySelectorAll('.info-header-tab'),
+      info = document.querySelector('.info-header'),
+      tabContent = document.querySelectorAll('.info-tabcontent'),
+
+  // Скрываем контент
+  function hideTabContent(a) {
+    for (let i = a; i < tabContent.length; i++) {
+      tabContent[i].classList.remove('show');
+      tabContent[i].classList.add('hide');
+    }
+  }
+
+  hideTabContent(1);
+
+  // Отображаем контент 
+  function showTabContent(b) {
+    if (tabContent[b].classList.contains('hide')) {
+      tabContent[b].classList.remove('hide');
+      tabContent[b].classList.add('show');
+    }
+  }
+
+  // Вешаем событие на кнопки, при клике на кнопку, открываем соответствующий таб
+  info.addEventListener('click', (e) => {
+    let target = e.target; 
+    if (target && target.classList.contains('info-header-tab')) {
+      for (let i = 0; i < tab.length; i++) {
+        if (target == tab[i]) {
+          hideTabContent(0);
+          showTabContent(i);
+          break;
+        }
+      }
+    }
+  });
+
+
+
+});
